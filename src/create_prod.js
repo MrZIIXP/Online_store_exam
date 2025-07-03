d0ccc1e6901d485e4dab225d44336e57de1af598
MrZIIXP <ziyodulloakhrorov@gmail.com>
2025-07-03 21:18:27 +0500

    done project

--- src/create_prod.js
+++ src/create_prod.js
@@ -1,32 +1,29 @@
 import { Apis } from './asunc.js'
 import { data_color } from './main.js'
 
-let AddForm = document.querySelector('#Add_product_from')
-AddForm.onsubmit = async e => {
+let AddForm = document.querySelector("#Add_product_from")
+AddForm.onsubmit = (e) => {
 	e.preventDefault()
 	let obj = {
-		name: AddForm['Name'].value,
-		img: AddForm['img'].value,
-		info: AddForm['desc'].value,
-		count: AddForm['count'].value,
-		company: AddForm['company'].value,
-		color: data_color,
+		name: AddForm["Name"].value,
+		img: AddForm["img"].value,
+		info: AddForm["desc"].value,
+		count: AddForm["count"].value,
+		company: AddForm["company"].value,
+		color: data_color
 	}
-	if (
-		obj.name &&
-		obj.img &&
-		obj.info &&
-		obj.count &&
-		obj.company &&
-		obj.color
-	) {
-		await new Apis().Post(obj)
+	if(obj.name, obj.img, obj.info, obj.count, obj.company, obj.color){
+		new Apis().Post(obj)
+		AddForm.reset()
+		document.querySelector("#Colors").innerHTML = ""
+		document.querySelector("#IMG").src = ""
 	}
 }
 
-AddForm['img'].oninput = () => {
-	document.querySelector('#IMG').src = AddForm['img'].value
+AddForm["img"].oninput = () => {
+	document.querySelector("#IMG").src = AddForm["img"].value	
 }
-document.querySelector('#IMG').onerror = () => {
-	document.querySelector('#IMG').src = ''
+document.querySelector("#IMG").onerror = () => {
+	document.querySelector("#IMG") = ""
 }
+
